9f4be131b5c1d032966c2f1f71f4c2ae2b7292bf
Tanesal <dguchinsky@yahoo.com>
2021-11-11 22:15:57 +0600

    refactor: prepare structure

--- bin/index.js
+++ bin/index.js
@@ -1,1 +1,14 @@
-console.log("Hello, this is cipher tool");
+#!/usr/bin/env node
+
+const yargs = require("yargs");
+
+const options = yargs.usage("Usage: -n <name>").option("n", {
+  alias: "name",
+  describe: "Your name",
+  type: "string",
+  demandOption: true,
+}).argv;
+
+const greeting = `Hello, ${options.name}!`;
+
+console.log(greeting);
